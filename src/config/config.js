ea13ec11a3c274b157818a82131e20bf70259cb6
MiguelSarmientoo <miguel.sarmiento@tecsup.edu.pe>
2024-12-11 05:43:36 -0500

    commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,10 @@
 // config.js
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
-  API_URL: 'https://54c8-76-137-84-85.ngrok-free.app',
-  GOOGLE_SPEECH_TO_TEXT_API_KEY: process.env.GOOGLE_SPEECH_TO_TEXT_API_KEY, // Asegúrate de tener un archivo .env configurado en el backend
+  API_URL: process.env.API_URL, // Cargar la variable de entorno
+  GOOGLE_SPEECH_TO_TEXT_API_KEY: process.env.GOOGLE_SPEECH_TO_TEXT_API_KEY, // Ya la tienes configurada
 };
 
 export default config;
